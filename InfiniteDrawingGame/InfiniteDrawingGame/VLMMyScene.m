3c3230ebd0e4290c3b10d18fef5ca4be525a3901
David <david@velluminous.org>
2013-12-09 13:39:39 -0800

    switching to a more performant drawing technique (adding textures to scene, rather than vector) happy accident bug here

--- InfiniteDrawingGame/InfiniteDrawingGame/VLMMyScene.m
+++ InfiniteDrawingGame/InfiniteDrawingGame/VLMMyScene.m
@@ -22,6 +22,7 @@ static const CGFloat kPlayerMovementSpeed = 100.0f;
 @property (nonatomic) CGPoint deadZone;
 @property (nonatomic) CGPoint maxVelocity;
 @property (nonatomic) CGPoint playerPosition;
+@property (nonatomic, strong) NSMutableDictionary *tileLookup;
 @end
 
 @implementation VLMMyScene
@@ -34,7 +35,8 @@ static const CGFloat kPlayerMovementSpeed = 100.0f;
         self.world = [SKNode node];
         [self addChild:self.world];
         self.playerPosition = CGPointZero;
-
+        
+        self.tileLookup = [[NSMutableDictionary alloc] init];
     }
     return self;
 }
@@ -59,19 +61,19 @@ static const CGFloat kPlayerMovementSpeed = 100.0f;
             // when the pan starts or ends, make sure we reset the state
         case UIGestureRecognizerStateBegan:
             [self setTargetVelocity:CGPointZero];
-            NSLog(@"pan began");
+            //NSLog(@"pan began");
             break;
         case UIGestureRecognizerStateEnded:
             [self setTargetVelocity:CGPointZero];
-            NSLog(@"pan began");
+            //NSLog(@"pan began");
             break;
         case UIGestureRecognizerStateCancelled:
             [self setTargetVelocity:CGPointZero];
-            NSLog(@"pan canceled");
+            //NSLog(@"pan canceled");
             break;
         default:
             //self.playerVelocity = [pgr translationInView:self.view];
-            NSLog(@"translation: %@", NSStringFromCGPoint([pgr translationInView:self.view]));
+            //NSLog(@"translation: %@", NSStringFromCGPoint([pgr translationInView:self.view]));
             [self processPanGestureRecognizer:pgr];
             
             break;
@@ -109,6 +111,19 @@ static const CGFloat kPlayerMovementSpeed = 100.0f;
     [self setTargetVelocity:CGPointZero];
     
 }
+-(BOOL)checkboundsfornodeposition:(CGPoint)nodepos reference:(CGPoint)ref{
+    NSInteger pad = 1;
+    if (fabsf(fabsf(nodepos.x)-fabsf(ref.x))>pad)
+    {
+        return YES;
+    }
+    if (fabsf(fabsf(nodepos.y)-fabsf(ref.y))>pad)
+    {
+        return YES;
+    }
+    
+    return NO;
+}
 -(void)update:(CFTimeInterval)currentTime {
 
     // Calculate the time since last update
@@ -132,19 +147,103 @@ static const CGFloat kPlayerMovementSpeed = 100.0f;
     self.playerPosition = CGPointMake(self.playerPosition.x + self.playerVelocity.x * timeSinceLast * kPlayerMovementSpeed,
                                        self.playerPosition.y + self.playerVelocity.y * timeSinceLast * kPlayerMovementSpeed);
     
-    if ( self.playerVelocity.x != 0.0f )
+    // loop through tiles in the scene and remove any that are out of bounds
+    ///
+    
+    CGPoint tilesize = CGPointMake(512.0f, 512.0f);
+    NSInteger column = floor(prev.x/tilesize.x);
+    NSInteger row = floor(prev.y/tilesize.y);
+    if ( self.playerVelocity.x != 0.0f || self.playerVelocity.y != 0.0f)
     {
+        /*
+        [self.world enumerateChildNodesWithName:@"mew" usingBlock: ^(SKNode *node, BOOL *stop) {
+            //NSLog(@"child %@", NSStringFromCGPoint(node.position));
+            
+            CGFloat col_ = floorf(node.position.x/tilesize.x);
+            CGFloat row_ = floorf(node.position.y/tilesize.y);
+            if ([self checkboundsfornodeposition:CGPointMake(col_, row_) reference:CGPointMake(column, row)])
+            {
+                NSLog(@"remov");
+                [node removeFromParent];
+            }
+        }];
+         */
+        
         CGPoint cur = self.playerPosition;
         SKShapeNode *yourline = [SKShapeNode node];
         CGMutablePathRef pathToDraw = CGPathCreateMutable();
+        /*
+        CGPathMoveToPoint(pathToDraw, NULL, prev.x - column*tilesize.x, prev.y - row*tilesize.y);
+        CGPathAddLineToPoint(pathToDraw, NULL, cur.x - column*tilesize.x, cur.y - row*tilesize.y);
+        */
+        /*
         CGPathMoveToPoint(pathToDraw, NULL, prev.x, prev.y);
         CGPathAddLineToPoint(pathToDraw, NULL, cur.x, cur.y);
-        yourline.path = pathToDraw;
+        [yourline setPath:pathToDraw];
         [yourline setLineWidth:1.0f];
         [yourline setFillColor:[UIColor clearColor]];
-        [yourline setStrokeColor:[UIColor redColor]];
-        yourline.antialiased = NO;
-        [self.world addChild:yourline];
+        [yourline setStrokeColor:[UIColor blackColor]];
+        [yourline setAntialiased:NO];
+         */
+        CGPoint offset = CGPointZero;
+        CGSize sizeroonie = CGSizeZero;
+        offset.x = cur.x < prev.x ? cur.x : prev.x;
+        offset.y = cur.y < prev.y ? cur.y : prev.y;
+        sizeroonie.width = fabsf(cur.x-prev.x);
+        sizeroonie.height = fabsf(cur.y-prev.y);
+        if (sizeroonie.width<1) {
+            sizeroonie.width = 1;
+        }
+        if (sizeroonie.height<1) {
+            sizeroonie.height = 1;
+        }
+        
+        UIGraphicsBeginImageContext(sizeroonie);
+        CGContextRef ctx = UIGraphicsGetCurrentContext();
+        [[SKColor blackColor] setFill];
+        //CGContextFillRect(ctx, CGRectMake(prev.x-offset.x, prev.y-offset.y, 1, 1));
+        //CGContextFillRect(ctx, CGRectMake(cur.x-offset.x, cur.y-offset.y, 1, 1));
+        CGPoint a = CGPointMake(prev.x-offset.x, prev.y-offset.y);
+        CGPoint b = CGPointMake(cur.x-offset.x, cur.y-offset.y);
+
+        [[SKColor blackColor] setStroke];
+        CGContextMoveToPoint(ctx, a.x, a.y);
+        CGContextAddLineToPoint(ctx, b.x, b.y);
+        CGContextDrawPath(ctx, kCGPathStroke);
+        
+        
+        UIImage *textureImage = UIGraphicsGetImageFromCurrentImageContext();
+        SKTexture *texture = [SKTexture textureWithImage:textureImage];
+        UIGraphicsEndImageContext();
+        
+        // request a tile (pull existing tile from dictionary or create one and add it to dictionary)
+        // tile is just a node
+        NSString *key = [NSString stringWithFormat:@"%i,%i", column, row];
+        SKNode *node;
+        if ([self.tileLookup objectForKey:key])
+        {
+            node = (SKNode *)[self.tileLookup objectForKey:key];
+            //[node setPosition:CGPointMake(column * tilesize.x, row * tilesize.y)];
+        }
+        else
+        {
+            node = [SKNode node];
+            [node setName:@"mew"];
+            [self.tileLookup setObject:node forKey:key];
+        }
+        SKSpriteNode *bg = [SKSpriteNode spriteNodeWithTexture:texture];
+        [bg setPosition:cur];
+        [node addChild:bg];
+
+        //[node addChild:yourline];
+
+        // add tilenode to scene if it isn't already in the scene?
+        if (!node.parent) {
+            [self.world addChild:node];
+            NSLog(@"adding");
+        }
+        
+        
     }
     // Move "camera" so the player is in the middle of the screen
     self.world.position = CGPointMake(-self.playerPosition.x + CGRectGetMidX(self.frame),
